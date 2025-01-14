a92e54f576422eeeec4d8875da26e20de6935640
DenysKavkalo <denysfalcons@gmail.com>
2025-01-14 20:10:14 +0000

    Benchmarks classes updated

--- Indexer/src/test/java/es/ulpgc/benchmark/IndexerBenchmark.java
+++ Indexer/src/test/java/es/ulpgc/benchmark/IndexerBenchmark.java
@@ -1,35 +1,35 @@
 package es.ulpgc.benchmark;
 
 import es.ulpgc.Cleaner.Book;
-import es.ulpgc.Indexer.BookIndexer;
+
 import es.ulpgc.Indexer.HashMapIndexer;
 import es.ulpgc.Indexer.Trie;
+import es.ulpgc.Indexer.Indexer;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Setup;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @State(org.openjdk.jmh.annotations.Scope.Thread)
 public class IndexerBenchmark {
 
-    private BookIndexer bookIndexer;
+    private Indexer indexer;
     private List<Book> books;
 
     @Setup
     public void setUp() {
-        // Prepare test data
+
         books = new ArrayList<>();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 2000; i++) {
             List<String> words = new ArrayList<>();
             for (int j = 0; j < 100; j++) {
                 words.add("word" + j);
             }
             books.add(new Book("Book" + i, "Author" + i, "2025", "English", "Credits", "ebook" + i, words, ""));
         }
-        bookIndexer = new BookIndexer();
+        indexer = new Indexer();
     }
 
     @Benchmark
@@ -53,9 +53,7 @@ public class IndexerBenchmark {
     }
 
     @Benchmark
-    public void benchmarkBookIndexer() {
-        for (Book book : books) {
-            bookIndexer.indexBook(book);
-        }
+    public void benchmarkIndexer() {
+        indexer.buildIndexes(books);
     }
 }
--- Indexer_CSV/src/test/java/es/ulpgc/benchmark/IndexerCSVBenchmark.java
+++ Indexer_CSV/src/test/java/es/ulpgc/benchmark/IndexerCSVBenchmark.java
@@ -1,4 +1,69 @@
 package es.ulpgc.benchmark;
 
+import es.ulpgc.Cleaner.Book;
+import es.ulpgc.Indexer.HashMapIndexer;
+import es.ulpgc.Indexer.Trie;
+import es.ulpgc.Indexer.Indexer;
+import es.ulpgc.Cleaner.Cleaner;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Setup;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@State(org.openjdk.jmh.annotations.Scope.Thread)
 public class IndexerCSVBenchmark {
+
+    private Indexer indexer;
+    private List<Book> books;
+
+    @Setup
+    public void setUp() throws IOException {
+        // Create test books similar to the second example
+        books = new ArrayList<>();
+        for (int i = 0; i < 2000; i++) {
+            List<String> words = new ArrayList<>();
+            for (int j = 0; j < 100; j++) {
+                words.add("word" + j);
+            }
+            books.add(new Book("Book" + i, "Author" + i, "2025", "English", "Credits", "ebook" + i, words, ""));
+        }
+        indexer = new Indexer();  // Use the Indexer for benchmarking
+    }
+
+    @Benchmark
+    public void benchmarkIndexerCSV() {
+        // Call indexBooks to save the results in CSV format
+        indexer.indexBooks(books, "csv");
+    }
+
+    @Benchmark
+    public void benchmarkHashMapIndexer() {
+        // Benchmark HashMapIndexer performance
+        HashMapIndexer hashMapIndexer = new HashMapIndexer();
+        for (Book book : books) {
+            for (String word : book.words) {
+                hashMapIndexer.addWord(word, book.ebookNumber);
+            }
+        }
+    }
+
+    @Benchmark
+    public void benchmarkTrieIndexer() {
+        // Benchmark Trie performance
+        Trie trie = new Trie();
+        for (Book book : books) {
+            for (String word : book.words) {
+                trie.insert(word, book.ebookNumber);
+            }
+        }
+    }
+
+    @Benchmark
+    public void benchmarkIndexer() {
+        // Call buildIndexes for benchmarking the parallel indexer
+        indexer.buildIndexes(books);
+    }
 }
